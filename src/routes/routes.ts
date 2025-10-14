efee0f87a1f9ff86d771e2f30c89f29dd900a979
Awais, Muhammad <106745198+977mawais@users.noreply.github.com>
2025-10-14 23:25:52 +0200

    feat: integrate all routes into main routes configuration

---
+++ src/routes/routes.ts
@@ -0,0 +1,18 @@
+import { Application } from 'express';
+import authRoutes from './auth.routes';
+import indexRoutes from './index.routes';
+import knowledgeRoutes from './knowledge.routes';
+import collectionRoutes from './collection.routes';
+import SearchRoutes from './search.routes';
+import AnalyticsRoutes from './analytics.routes';
+
+export default class Routes {
+  constructor(app: Application) {
+    app.use('/', indexRoutes);
+    app.use('/api/auth', authRoutes);
+    app.use('/api/knowledge', knowledgeRoutes);
+    app.use('/api/collections', collectionRoutes);
+    app.use('/api/search', SearchRoutes);
+    app.use('/api/analytics', AnalyticsRoutes);
+  }
+}
