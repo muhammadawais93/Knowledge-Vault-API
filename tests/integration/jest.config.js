ba876dcb4ed5adc29c3d4d3a9471efdfde453024
Awais, Muhammad <106745198+977mawais@users.noreply.github.com>
2025-12-26 02:31:03 +0100

    fix(tests): update paths in Jest configuration for integration tests

--- tests/integration/jest.config.js
+++ tests/integration/jest.config.js
@@ -4,9 +4,9 @@ module.exports = {
   ...baseConfig,
   maxWorkers: 1,
   testMatch: ['**/tests/integration/**/*.spec.ts'],
-  globalSetup: './setup/globalSetup.ts',
-  globalTeardown: './setup/globalTeardown.ts',
-  setupFiles: ['./setup/env.ts'],
-  setupFilesAfterEnv: ['./setup/setupTests.ts'],
+  globalSetup: '<rootDir>/tests/integration/setup/globalSetup.ts',
+  globalTeardown: '<rootDir>/tests/integration/setup/globalTeardown.ts',
+  setupFiles: ['<rootDir>/tests/integration/setup/env.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/integration/setup/setupTests.ts'],
   coverageDirectory: 'coverage/integration',
 };
